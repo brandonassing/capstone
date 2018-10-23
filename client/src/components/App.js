921e7d9e62bd99734b937921b7a0bd756b15da8a
Brandon Assing <assing.brandon@gmail.com>
2018-10-23 17:10:36 -0400

    Added basic react/redux/redux-thunk/redux-router. Ref used: https://medium.com/@notrab/getting-started-with-create-react-app-redux-react-router-redux-thunk-d6a19259f71f

---
+++ client/src/components/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import logo from '../logo.svg';
+import './App.css';
+import { Route, Link } from 'react-router-dom';
+
+import Home from './Home';
+import About from './About';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+        </header>
+
+        <main>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;
