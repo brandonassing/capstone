def1b4bca79407be98854de69e06690ccd7cda86
Brandon Assing <assing.brandon@gmail.com>
2018-12-25 16:08:43 -0400

    added table styling

--- client/src/components/ClientProfile.js
+++ client/src/components/ClientProfile.js
@@ -20,7 +20,7 @@ class ClientProfile extends Component {
           <h2>Client profiles</h2>
           <input type="email" className="form-control" id="client-search" placeholder="Search" />
         </div>
-        <table className="table table-hover">
+        <table id="client-profile-table" className="table table-hover">
           <thead>
             <tr>
               <th scope="col">id</th>
@@ -36,20 +36,31 @@ class ClientProfile extends Component {
               this.props.clientProfiles.map((client) => {
                 let mostRecent = 0;
                 let mostRecentIndex = 0;
+                let churnClass = "";
+
                 for (let i = 0; i < client.churnProbabilities.length; i++) {
                   if(client.churnProbabilities[i].timestamp > mostRecent) {
                     mostRecent = client.churnProbabilities[i].timestamp;
                     mostRecentIndex = i;
                   }
                 }
+                if (client.churnProbabilities[mostRecentIndex].probability >= 0 && client.churnProbabilities[mostRecentIndex].probability < 40) {
+                  churnClass = "good";
+                }
+                else if (client.churnProbabilities[mostRecentIndex].probability >= 40 && client.churnProbabilities[mostRecentIndex].probability < 75) {
+                  churnClass = "med";
+                }
+                else if (client.churnProbabilities[mostRecentIndex].probability >= 75 && client.churnProbabilities[mostRecentIndex].probability <= 100) {
+                  churnClass = "bad";
+                }
                 return(
                   <tr key={client.clientId}>
-                    <th scope="row">{client.clientId}</th>
-                    <td>{client.firstName} {client.lastName}</td>
-                    <td>{client.email}</td>
-                    <td>{client.phoneNumber}</td>
-                    <td>{client.planDetails.length}</td>
-                    <td>{client.churnProbabilities[mostRecentIndex].probability}</td>
+                    <th scope="row"><p>{client.clientId}</p></th>
+                    <td><p>{client.firstName} {client.lastName}</p></td>
+                    <td><p>{client.email}</p></td>
+                    <td><p>{client.phoneNumber}</p></td>
+                    <td><p>{client.planDetails.length}</p></td>
+                    <td className="churn-prob"><p className={churnClass}>{client.churnProbabilities[mostRecentIndex].probability}</p></td>
                   </tr>
                 );
               })
