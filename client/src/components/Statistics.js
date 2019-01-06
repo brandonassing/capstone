7ea5591b0f5c3c49d8262a5856c7ac659c849983
Brandon Assing <assing.brandon@gmail.com>
2019-01-06 10:17:14 -0500

    added client data to sample chart

--- client/src/components/Statistics.js
+++ client/src/components/Statistics.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './Statistics.scss';
 import { Line } from 'react-chartjs-2';
+import moment from 'moment';
+
 
 class Statistics extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      churn: []
+      churns: [],
+      timestamps: [],
+      probabilities: []
     }
   }
   componentDidMount() {
@@ -14,31 +18,69 @@ class Statistics extends Component {
       .then(res => res.json())
       .then(resJson => {
         let churns = [];
+        let timestamps = [];
+        let probabilities = [];
         for (let i = 0; i < resJson.message.length; i++) {
           churns = [...churns, ...resJson.message[i].churnProbabilities];
         }
+        churns.sort((a,b) => (moment(a.timestamp).isAfter(b.timestamp)) ? 1 : ((moment(b.timestamp).isAfter(a.timestamp)) ? -1 : 0)); 
+
+        for (let i = 0; i < churns.length; i++) {
+          timestamps.push(churns[i].timestamp);
+          probabilities.push(churns[i].probability);
+        }
         this.setState({
-          churn: churns
+          churns: churns,
+          timestamps: timestamps,
+          probabilities: probabilities
         });
       });
   }
   render() {
     var data = {
-      labels: ["January", "February", "March", "April", "May", "June", "July"],
+      labels: this.state.timestamps,
       datasets: [{
-        label: "My First dataset",
+        fill: false,
+        label: "Churn probability",
         backgroundColor: 'rgb(255, 99, 132)',
         borderColor: 'rgb(255, 99, 132)',
-        data: [0, 10, 5, 2, 20, 30, 45],
+        data: this.state.probabilities,
       }]
     }
+    var graphOptions = {
+      scales: {
+        xAxes: [{
+          type: 'time',
+          time: {
+            displayFormats: {
+              'millisecond': 'MMM DD',
+              'second': 'MMM DD',
+              'minute': 'MMM DD',
+              'hour': 'MMM DD',
+              'day': 'MMM DD',
+              'week': 'MMM DD',
+              'month': 'MMM DD',
+              'quarter': 'MMM DD',
+              'year': 'MMM DD',
+            },
+            tooltipFormat: 'DD/MM/YY'
+          }
+        }],
+      },
+      elements: {
+        line: {
+            tension: 0, // disables bezier curves
+        }
+      }
+    };
     return (
       <div>
         <div id="line-graph">
           < Line
             data={data}
-            height={500}
+            height={300}
             width={700}
+            options={graphOptions}
           />
         </div>
       </div>
