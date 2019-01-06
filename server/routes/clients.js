fd5ce9d8ffd854978b4e1656e0d3ec0f76143ed8
Brandon Assing <assing.brandon@gmail.com>
2019-01-06 07:10:11 -0500

    idk

--- server/routes/clients.js
+++ server/routes/clients.js
@@ -4,38 +4,51 @@ var router = express.Router();
 var Client = require('../models/client');
 
 router.get('/', function(req, res, next) {
-    res.json({
-        message: 'welcome to our clients api!'
-    });
+  res.json({
+    message: 'welcome to our clients api!'
+  });
 });
 
 router.route('/profiles')
-.get(function (req, res) {
-  var pageNo = parseInt(req.query.pageNo)
-  var size = parseInt(req.query.size)
-  var query = {}
-  if(pageNo < 0 || pageNo === 0) {
-        response = {"error" : true,"message" : "invalid page number, should start with 1"};
-        return res.json(response)
-  }
-  query.skip = size * (pageNo - 1)
-  query.limit = size
-  // Find some documents
-       Client.estimatedDocumentCount({},function(err,totalCount) {
-             if(err) {
-               response = {"error" : true,"message" : "Error fetching data"}
-             }
-         Client.find({},{},query,function(err,data) {
-              // Mongo command to fetch all data from collection.
-            if(err) {
-                response = {"error" : true,"message" : "Error fetching data"};
-            } else {
-                var totalPages = Math.ceil(totalCount / size)
-                response = {"error" : false,"message" : data,"pages": totalPages};
-            }
-            res.json(response);
-         });
-       })
-});
+  .get(function(req, res) {
+    var pageNo = parseInt(req.query.pageNo);
+    var size = parseInt(req.query.size);
+    var query = {};
+    if (pageNo < 0 || pageNo === 0) {
+      response = {
+        "error": true,
+        "message": "invalid page number, should start with 1"
+      };
+      return res.json(response);
+    }
+    query.skip = size * (pageNo - 1);
+    query.limit = size;
+    // Find some documents
+    Client.estimatedDocumentCount({}, function(err, totalCount) {
+      if (err) {
+        response = {
+          "error": true,
+          "message": "Error fetching data"
+        };
+      }
+      Client.find({}, {}, query, function(err, data) {
+        // Mongo command to fetch all data from collection.
+        if (err) {
+          response = {
+            "error": true,
+            "message": "Error fetching data"
+          };
+        } else {
+          var totalPages = Math.ceil(totalCount / size);
+          response = {
+            "error": false,
+            "message": data,
+            "pages": totalPages
+          };
+        }
+        res.json(response);
+      });
+    });
+  });
 
 module.exports = router;
