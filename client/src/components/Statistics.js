d2a3116b302486f78f6475ad9ee7017a531a0bd3
Brandon Assing <assing.brandon@gmail.com>
2019-01-06 08:27:34 -0500

    churn retrieval working

--- client/src/components/Statistics.js
+++ client/src/components/Statistics.js
@@ -2,26 +2,44 @@ import React, { Component } from 'react';
 import './Statistics.scss';
 import { Line } from 'react-chartjs-2';
 
-
 class Statistics extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      churn: []
+    }
+  }
+  componentDidMount() {
+    fetch('/clients/churn')
+      .then(res => res.json())
+      .then(resJson => {
+        let churns = [];
+        for (let i = 0; i < resJson.message.length; i++) {
+          churns = [...churns, ...resJson.message[i].churnProbabilities];
+        }
+        this.setState({
+          churn: churns
+        });
+      });
+  }
   render() {
-    var data= {
-        labels: ["January", "February", "March", "April", "May", "June", "July"],
-        datasets: [{
+    var data = {
+      labels: ["January", "February", "March", "April", "May", "June", "July"],
+      datasets: [{
         label: "My First dataset",
         backgroundColor: 'rgb(255, 99, 132)',
         borderColor: 'rgb(255, 99, 132)',
         data: [0, 10, 5, 2, 20, 30, 45],
-        }]
+      }]
     }
     return (
       <div>
         <div id="line-graph">
-        < Line
-        data={data}
-        height={500}
-        width={700}
-        />
+          < Line
+            data={data}
+            height={500}
+            width={700}
+          />
         </div>
       </div>
     );
@@ -29,3 +47,19 @@ class Statistics extends Component {
 }
 
 export default Statistics;
+
+
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     storeClients: clientData => dispatch(storeClients(clientData)),
+//     refreshClients: clientData => dispatch(refreshClients(clientData))
+//   };
+// };
+
+// const mapStateToProps = state => {
+//   return {
+//     clientProfiles: state.clientReducer.clients
+//   };
+// };
+
+// export default connect(mapStateToProps, mapDispatchToProps)(Statistics)
