034c4496899792ebeec19f8f4379b804e2b4eb97
Brandon Assing <assing.brandon@gmail.com>
2019-01-06 08:27:34 -0500

    churn retrieval working

--- server/routes/clients.js
+++ server/routes/clients.js
@@ -3,52 +3,69 @@ var router = express.Router();
 // var clients = require('../devSamples/clients').clients;
 var Client = require('../models/client');
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.json({
     message: 'welcome to our clients api!'
   });
 });
 
-router.route('/profiles')
-  .get(function(req, res) {
-    var pageNo = parseInt(req.query.pageNo);
-    var size = parseInt(req.query.size);
-    var query = {};
-    if (pageNo < 0 || pageNo === 0) {
+router.route('/profiles').get(function (req, res) {
+  var pageNo = parseInt(req.query.pageNo);
+  var size = parseInt(req.query.size);
+  var query = {};
+  if (pageNo < 0 || pageNo === 0) {
+    response = {
+      "error": true,
+      "message": "invalid page number, should start with 1"
+    };
+    return res.json(response);
+  }
+  query.skip = size * (pageNo - 1);
+  query.limit = size;
+  // Find some documents
+  Client.estimatedDocumentCount({}, function (err, totalCount) {
+    if (err) {
       response = {
         "error": true,
-        "message": "invalid page number, should start with 1"
+        "message": "Error fetching data"
       };
-      return res.json(response);
     }
-    query.skip = size * (pageNo - 1);
-    query.limit = size;
-    // Find some documents
-    Client.estimatedDocumentCount({}, function(err, totalCount) {
+    Client.find({}, {}, query, function (err, data) {
+      // Mongo command to fetch all data from collection.
       if (err) {
         response = {
           "error": true,
           "message": "Error fetching data"
         };
+      } else {
+        var totalPages = Math.ceil(totalCount / size);
+        response = {
+          "error": false,
+          "message": data,
+          "pages": totalPages
+        };
       }
-      Client.find({}, {}, query, function(err, data) {
-        // Mongo command to fetch all data from collection.
-        if (err) {
-          response = {
-            "error": true,
-            "message": "Error fetching data"
-          };
-        } else {
-          var totalPages = Math.ceil(totalCount / size);
-          response = {
-            "error": false,
-            "message": data,
-            "pages": totalPages
-          };
-        }
-        res.json(response);
-      });
+      res.json(response);
     });
   });
+});
+
+router.route('/churn').get(function (req, res) {
+  Client.find({}, function (err, data) {
+    if (err) {
+      response = {
+        "error": true,
+        "message": "Error fetching data"
+      };
+    }
+    else {
+      response = {
+        "error": false,
+        "message": data
+      }
+    }
+    res.json(response);
+  }).select('churnProbabilities'); 
+});
 
 module.exports = router;
