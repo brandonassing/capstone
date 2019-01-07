c878e499f930ad5aa4abf348d302ada32c56660e
Brandon Assing <assing.brandon@gmail.com>
2019-01-07 08:31:26 -0500

    added charts

--- client/src/components/Statistics.js
+++ client/src/components/Statistics.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './Statistics.scss';
-import { Line } from 'react-chartjs-2';
+import { Line, Pie } from 'react-chartjs-2';
 import moment from 'moment';
 
 
@@ -12,6 +12,8 @@ class Statistics extends Component {
       timestamps: [],
       probabilities: []
     }
+
+    this.getDonutData = this.getDonutData.bind(this);
   }
   componentDidMount() {
     fetch('/clients/churn')
@@ -23,7 +25,7 @@ class Statistics extends Component {
         for (let i = 0; i < resJson.message.length; i++) {
           churns = [...churns, ...resJson.message[i].churnProbabilities];
         }
-        churns.sort((a,b) => (moment(a.timestamp).isAfter(b.timestamp)) ? 1 : ((moment(b.timestamp).isAfter(a.timestamp)) ? -1 : 0)); 
+        churns.sort((a, b) => (moment(a.timestamp).isAfter(b.timestamp)) ? 1 : ((moment(b.timestamp).isAfter(a.timestamp)) ? -1 : 0));
 
         for (let i = 0; i < churns.length; i++) {
           timestamps.push(churns[i].timestamp);
@@ -36,8 +38,29 @@ class Statistics extends Component {
         });
       });
   }
+
+  getDonutData() {
+    let good = 0;
+    let med = 0;
+    let bad = 0;
+
+    this.state.probabilities.forEach((e) => {
+      if (e >= 0 && e < 40) {
+        good++;
+      }
+      else if (e >= 40 && e < 75) {
+        med++;
+      }
+      else if (e >= 75 && e < 100) {
+        bad++;
+      }
+    });
+
+    return [good, med, bad];
+  }
+
   render() {
-    var data = {
+    let lineData = {
       labels: this.state.timestamps,
       datasets: [{
         fill: false,
@@ -46,8 +69,8 @@ class Statistics extends Component {
         borderColor: 'rgb(255, 99, 132)',
         data: this.state.probabilities,
       }]
-    }
-    var graphOptions = {
+    };
+    let lineOptions = {
       scales: {
         xAxes: [{
           type: 'time',
@@ -69,19 +92,56 @@ class Statistics extends Component {
       },
       elements: {
         line: {
-            tension: 0, // disables bezier curves
+          tension: 0, // disables bezier curves
         }
       }
     };
+
+    let donutData = {
+      datasets: [{
+        data: this.getDonutData(),
+        backgroundColor: ["#34bc6e", "#fbeaae", "#e62325"]
+      }],
+      labels: ["Good", "Med", "Bad"]
+    };
+    let donutOptions = {
+      cutoutPercentage: 50,
+      animation: {
+        animateRotate: true
+      }
+    };
+
     return (
-      <div>
-        <div id="line-graph">
-          < Line
-            data={data}
-            height={300}
-            width={700}
-            options={graphOptions}
-          />
+      <div id="stats-body">
+        <div id="stats-header">
+          <h2>Statistics</h2>
+        </div>
+        <div id="stats-content">
+          <div id="line-graph">
+            <h3>Clients churn over time</h3>
+            <Line data={lineData} options={lineOptions} height={100} />
+          </div>
+          <div id="lower-group">
+            <div id="donut-graph">
+              <h3>Clients churn distribution</h3>
+              <Pie data={donutData} options={donutOptions} />
+            </div>
+            <div id="avg-group">
+              <h3>Clients churn averages</h3>
+              <div id="avg-cards">
+                <div className="avg">
+
+                </div>
+                <div className="avg">
+
+                </div>
+                <div className="avg">
+
+                </div>
+              </div>
+            </div>
+          </div>
+
         </div>
       </div>
     );
