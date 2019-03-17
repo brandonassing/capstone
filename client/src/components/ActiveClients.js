b5c78ab7ba4909c8922396bee6d85a2e24970b72
Brandon Assing <assing.brandon@gmail.com>
2019-03-17 16:07:34 -0400

    added invoice total to client and active

--- client/src/components/ActiveClients.js
+++ client/src/components/ActiveClients.js
@@ -244,7 +244,9 @@ class ActiveClients extends Component {
                           <p>Status: {item.status}</p>
                           <p>Invoice probability: <strong>{Math.round(item.opportunityProbability * 100)}%</strong></p>
                           <p>Value estimate: <strong className={item.estimateValue === 1 ? "low" : item.estimateValue === 2 ? "med" : "high"}>{item.estimateValue === 1 ? "Low" : item.estimateValue === 2 ? "Med" : "High"}</strong></p>
-                          {item.status === "completed" ? <p>Invoice: <strong>${item.invoice}</strong></p> : ""}
+                          {item.status === "completed" ? <p>Invoice total: <strong>${
+                            item.invoice.reduce((total, inv) => total + inv.amountAfterDiscount, 0)
+                          }</strong></p> : ""}
                         </div>
                         {item.status !== "completed" ?
                           <div className="worker-dropdown">
