d57e1871a88f5a00a9fcd87ddc8c6d30d10bbac1
Brandon Assing <assing.brandon@gmail.com>
2019-03-17 16:03:48 -0400

    invoice finished ish

--- client/src/_helpers/errors.js
+++ client/src/_helpers/errors.js
@@ -1,5 +1,5 @@
-import { userActions } from '../_actions/user';
-import store from '../store';
+// import { userActions } from '../_actions/user';
+// import store from '../store';
 export const handleError = (response) => {
     if (!response.ok) {
         if (response.status === 401) {
