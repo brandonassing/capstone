47500820d5168434356089d2e8cf942f96248950
Brandon Assing <assing.brandon@gmail.com>
2019-03-19 00:09:11 -0400

    removed getdata from nav and altered graph to only last 6 months

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -2,14 +2,11 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './Nav.scss';
 
-import moment from 'moment';
 import { connect } from 'react-redux';
 import { storeMetrics, removeMetrics } from '../_actions/metricList';
 import { removeAllClients } from '../_actions/clientList';
 import { removeUsers } from '../_actions/user';
 
-import { authHeader } from '../_helpers/auth';
-import { handleError } from '../_helpers/errors';
 const logo = require('../assets/Logo.png');
 
 class Nav extends Component {
@@ -18,74 +15,6 @@ class Nav extends Component {
     this.state = {
       user: {}
     }
-    this.getStats = this.getStats.bind(this);
-  }
-
-  // TODO is this necessary?
-  getStats() {
-    fetch('/clients/calls', {
-      method: 'GET',
-      headers: authHeader()
-    })
-      .then(res => handleError(res))
-      .then(res => res.json())
-      .then(resJson => {
-        let calls = [];
-        let timestamps = [];
-        let probabilities = [];
-        let estimateValues = [];
-        let invoices = [];
-
-        for (let i = 0; i < resJson.message.length; i++) {
-          calls = [...calls, ...resJson.message[i].calls];
-        }
-        calls.sort((a, b) => (moment(a.timestamp).isAfter(b.timestamp)) ? 1 : ((moment(b.timestamp).isAfter(a.timestamp)) ? -1 : 0));
-
-        for (let i = 0; i < calls.length; i++) {
-          timestamps.push(calls[i].timestamp.slice(0, 10));
-          probabilities.push(Math.round(calls[i].opportunityProbability * 100));
-          estimateValues.push(calls[i].estimateValue);
-          let invoiceTotal = calls[i].invoice.reduce((total, inv) => total + inv.amountAfterDiscount, 0);
-          invoices.push(invoiceTotal);
-        }
-
-        let timesObject = {};
-
-        for (let i = 0; i < timestamps.length; i++) {
-          if (!timesObject.hasOwnProperty(timestamps[i])) {
-            timesObject[timestamps[i]] = [invoices[i]];
-          }
-          else {
-            timesObject[timestamps[i]] = [...timesObject[timestamps[i]], invoices[i]]
-          }
-        }
-
-        for (let prop in timesObject) {
-          let sum = 0;
-          for (let i = 0; i < timesObject[prop].length; i++) {
-            sum += timesObject[prop][i];
-          }
-          let avg = sum / timesObject[prop].length;
-          timesObject[prop] = avg;
-        }
-
-        timestamps = Object.keys(timesObject);
-        let metricAvg = timestamps.map((item) => {
-          return parseInt((Math.round(timesObject[item] * 100) / 100).toFixed(2));
-        });
-
-        this.props.storeMetrics({
-          calls: calls,
-          timestamps: timestamps,
-          metricAvg: metricAvg,
-          probabilities: probabilities,
-          estimateValues: estimateValues,
-          invoices: invoices
-        });
-      })
-      .catch(err => {
-        console.log(err)
-      });
   }
 
   componentDidMount() {
@@ -102,7 +31,7 @@ class Nav extends Component {
           </div>
           <ul className="nav nav-pills" id="navTabs" role="tablist">
             <li className="nav-item">
-              <a className="nav-link active" data-toggle="tab" href="#stats" role="tab" aria-controls="stats" aria-selected="true" onClick={this.getStats}>Statistics</a>
+              <a className="nav-link active" data-toggle="tab" href="#stats" role="tab" aria-controls="stats" aria-selected="true">Statistics</a>
             </li>
             <li className="nav-item">
               <a className="nav-link" data-toggle="tab" href="#clients" role="tab" aria-controls="clients" aria-selected="false">Prospects</a>
