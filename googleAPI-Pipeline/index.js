b0e011c148fe221c0386b40f6fa8a39dd01ed672
Krishan Patel <krishanpatel09@gmail.com>
2019-03-20 18:12:16 -0400

    Google speech to text API completed

--- googleAPI-Pipeline/index.js
+++ googleAPI-Pipeline/index.js
@@ -1,348 +1,365 @@
-// Speech to Text translation program
+const csvFilePath = process.argv[2].toString() + process.argv[3].toString();
 
-// Initialize the csv to json module
-//const csvFilePath='./csv/2018_Finan_Call_data.csv'
-const csvFilePath='./csv/tr_processing.csv'
-
-
-const csv=require('csvtojson')
+const csv = require("csvtojson");
 
 // Imports the Google Cloud client library
-const {Storage} = require('@google-cloud/storage');
+const { Storage } = require("@google-cloud/storage");
 
 // Your Google Cloud Platform project ID
-const projectId = 'leadsense-230423';
+const projectId = "leadsense-230423";
 
 // HTTP configuration for downloading MP3 files
-const https = require('https');
-const fs = require('fs');
+const https = require("https");
+const fs = require("fs");
 
 // Import the json 2 csv converter
-const Json2csvParser = require('json2csv').Parser;
-//const fields = ['transcription', 'callId', 'date', 'time', 'duration','prospect','callStatus','callerNumber'];
-const fields = ["Unique Id","Call Id", "Words"]
+const Json2csvParser = require("json2csv").Parser;
+const fields = [
+  "Date",
+  "Time",
+  "Employee",
+  "Location",
+  "Extension Routing",
+  "Ad Source",
+  "Routed Type",
+  "Result",
+  "Duration",
+  "Status",
+  "Tracking Number",
+  "Caller Number",
+  "Target Number",
+  "Missed Opp",
+  "Prospect/Non-Prospect",
+  "Call Status",
+  "Call Id",
+  "Audio URL",
+  "Campaign Id",
+  "Campaign Name",
+  "Location Code",
+  "Location Id",
+  "First Name",
+  "Last Name",
+  "Street Address",
+  "City",
+  "State",
+  "Zip Code",
+  "Country",
+  "Appointment Set",
+  "Appointment Date and Time",
+  "Call Alert Status",
+  "Words"
+];
 
-const myCSV = []
-
-
-// For parsing bodies
-const express = require("express");
-
-const BodyParser = require("body-parser");
-
+const myCSV = [];
 
 // For indentifying audio files:
 // Must download the sox CLI to run first. For MAC, use home brew to download: https://brewinstall.org/Install-sox-on-Mac-with-Brew/
-var sox = require('sox');
+var sox = require("sox");
 
 // Imports the Google Cloud client library
-const speech = require('@google-cloud/speech').v1p1beta1;
-
-// Import Mongo stuff
-const mongoose = require('mongoose')
-const MongoClient = require("mongodb").MongoClient;
-const ObjectId = require("mongodb").ObjectID;
-//var creds = require("./config/config.js")
-mongoose.Promise = global.Promise
-
+const speech = require("@google-cloud/speech").v1p1beta1;
 
 // Create a speech client
 const client = new speech.SpeechClient();
 
-// Speech client configuration settings: 
+// Speech client configuration settings:
 const clientConfig = {
-  encoding: 'LINEAR16',
+  encoding: "LINEAR16",
   sampleRateHertz: 16000,
-  languageCode: 'en-US',
+  languageCode: "en-US",
   enableSpeakerDiarization: true,
   audioChannelCount: 2,
   enableSeparateRecognitionPerChannel: true,
   diarizationSpeakerCount: 2,
-  model: 'phone_call'
+  model: "phone_call"
 };
-
-// Grab the mongo schema:
-const Transcription = require("./models/transcription.js")
-
-// Express app stuff: -----------------------------------------------------------------------------
-var app = express();
-var router = express.Router()
-app.use(BodyParser.json());
-app.use(BodyParser.urlencoded({ extended: true }));
-var querystring = require("querystring");
-
 
 //convert()
 
-main()
+main();
 
 // global script variables ---------
-var once = false
-var i = 0
+var once = false;
+var i = 0;
 var jsonArray;
-var lastLink = false
+var lastLink = false;
 // global script variables ----------
 
-async function main(){
-console.log("Loading script")
+async function main() {
+  console.log("Loading script");
 
+  if (!once) {
+    jsonArray = await csv().fromFile(csvFilePath);
+  }
+  once = true;
 
-if(!once){
-  jsonArray = await csv().fromFile(csvFilePath);
-}
-once = true
+  // loop through all call url files
 
-// loop through all call url files
+  // Access the metadata of the call object:
+  var metadata = jsonArray[i];
+  var unique = i; //metadata['Unique Id']
+  var callId = metadata["Call Id"];
+  var dest = "./downloads/" + unique + ".mp3";
+  var url = metadata["Audio URL"];
 
-
-
-// Access the metadata of the call object: 
-var metadata = jsonArray[i]
-//console.log("the jsonArray: " + JSON.stringify(jsonArray[0]))
-//var dest = "./downloads/" + metadata['Call Id'] + ".mp3"
-var unique = i //metadata['Unique Id']
-var callId = metadata['Call Id']
-var dest = "./downloads/" + unique + ".mp3"
-var url = metadata['Audio URL']
-
-  await download(url, dest, callId, metadata, unique, function(x){
-
-    console.log("download mp3 #: " + i)
+  await download(url, dest, callId, metadata, unique, function(x) {
+    console.log("download mp3 #: " + i);
 
     // Set the limit of this script
-    if(i <= jsonArray.length - 1){
-      console.log("Last file")
-      lastLink = true
+    if (i <= jsonArray.length - 1) {
+      console.log("Last file");
+      lastLink = true;
     }
 
     // Convert the audio file into text
-    convertMP3toWAV(dest, callId,lastLink, metadata, unique, function(){
-     // console.log("Callback finished!")
+    convertMP3toWAV(dest, callId, lastLink, metadata, unique, function() {
+      // console.log("Callback finished!")
 
-        if(!lastLink){
-          console.log("not over yet")
-          i++
-          main()
-        }
-
-      })
-    })
+      if (!lastLink) {
+        console.log("not over yet");
+        i++;
+        main();
+      }
+    });
+  });
 }
-
 
 // Download the CSV file from call source
-async function download(url, dest,callId, metadata,unique, cb) {
+async function download(url, dest, callId, metadata, unique, cb) {
   var file = fs.createWriteStream(dest);
-  var request = https.get(url, function(response) {
-    response.pipe(file);
-    file.on('finish', function() {
-      file.close(cb);  // close() is async, call cb after close completes.
+  var request = https
+    .get(url, function(response) {
+      response.pipe(file);
+      file.on("finish", function() {
+        file.close(cb); // close() is async, call cb after close completes.
+      });
+    })
+    .on("error", function(err) {
+      // Handle errors
+      fs.unlink(dest); // Delete the file async. (But we don't check the result)
+      if (cb) cb(err.message);
     });
-  }).on('error', function(err) { // Handle errors
-    fs.unlink(dest); // Delete the file async. (But we don't check the result)
-    if (cb) cb(err.message);
-  });
-};
-
-
-async function convert2Flac(){
-
-  const ffmpeg = require('fluent-ffmpeg');
-  let track = './downloads/audioFile.mp3';//your path to source file
-
-  ffmpeg(track)
-  .toFormat('flac')
-  .audioChannels(2)
-  .audioFrequency(16000)
-  .audioBitrate('128k')
-  .on('error', (err) => {
-      console.log('An error occurred: ' + err.message);
-  })
-  .on('progress', (progress) => {
-      // console.log(JSON.stringify(progress));
-      console.log('Processing: ' + progress.targetSize + ' KB converted');
-  })
-  .on('end', () => {
-      console.log('Processing finished !');
-
-    // Upload to google bucket:
-    var googleBucketAddress = "./transcodes/please.flac"
-    upload2GoogleBucket(googleBucketAddress)
-
-  })
-  .save('./transcodes/please.flac');//path where you want to save your file
-
-
 }
 
+async function convert2Flac() {
+  const ffmpeg = require("fluent-ffmpeg");
+  let track = "./downloads/audioFile.mp3"; //your path to source file
+
+  ffmpeg(track)
+    .toFormat("flac")
+    .audioChannels(2)
+    .audioFrequency(16000)
+    .audioBitrate("128k")
+    .on("error", err => {
+      console.log("An error occurred: " + err.message);
+    })
+    .on("progress", progress => {
+      // console.log(JSON.stringify(progress));
+      console.log("Processing: " + progress.targetSize + " KB converted");
+    })
+    .on("end", () => {
+      console.log("Processing finished !");
+
+      // Upload to google bucket:
+      var googleBucketAddress = "./transcodes/please.flac";
+      upload2GoogleBucket(googleBucketAddress);
+    })
+    .save("./transcodes/please.flac"); //path where you want to save your file
+}
 
 // Use the SoX CLI to convert the mp3 file (from call source) into a .wav format
-async function convertMP3toWAV(dest, callId,lastLink, metadata,unique, cb) {
-
+async function convertMP3toWAV(dest, callId, lastLink, metadata, unique, cb) {
   // Define variable locations
-  var mp3File = dest
-  var outputDestiation = './wavFiles/' + unique + ".wav"
+  var mp3File = dest;
+  var outputDestiation = "./wavFiles/" + unique + ".wav";
 
+  sox.identify(mp3File, function(err, results) {
+    // Preview the details about the mp3 file:
+    console.log("Inside details: " + JSON.stringify(results));
 
-   sox.identify(mp3File, function(err, results) {
-    
-      // Preview the details about the mp3 file:
-      console.log("Inside details: " + JSON.stringify(results))
-      
-      var job = sox.transcode(mp3File, outputDestiation, {
-        sampleRate: 16000,
-        format: 'wav',
-        channelCount: 2,
-       // bitRate: 16384,
-        compressionQuality: 8, // see `man soxformat` search for '-C' for more info
-      });
-      job.on('error', function(err) {
-        console.error(err);
-      });
-      job.on('progress', function(amountDone, amountTotal) {
-        console.log("progress", amountDone, amountTotal);
-      });
-      job.on('src', function(info) {});
-      job.on('dest', function(info) {
-        console.log("WAV file details: " + JSON.stringify(info))
-      });
-      job.on('end', function() {
-        console.log("Conversion complete.");
-        // Upload the file location to Google Cloud Storage
-        
-         upload2GoogleBucket(outputDestiation, callId,lastLink, metadata,unique, function(){
-
-          cb()
-         })
-         
-      });
-      job.start();
+    var job = sox.transcode(mp3File, outputDestiation, {
+      sampleRate: 16000,
+      format: "wav",
+      channelCount: 2,
+      // bitRate: 16384,
+      compressionQuality: 8 // see `man soxformat` search for '-C' for more info
     });
+    job.on("error", function(err) {
+      console.error(err);
+    });
+    job.on("progress", function(amountDone, amountTotal) {
+      console.log("progress", amountDone, amountTotal);
+    });
+    job.on("src", function(info) {});
+    job.on("dest", function(info) {
+      console.log("WAV file details: " + JSON.stringify(info));
+    });
+    job.on("end", function() {
+      console.log("Conversion complete.");
+      // Upload the file location to Google Cloud Storage
+
+      upload2GoogleBucket(
+        outputDestiation,
+        callId,
+        lastLink,
+        metadata,
+        unique,
+        function() {
+          cb();
+        }
+      );
+    });
+    job.start();
+  });
 }
 
 // Upload the convert file to Google Cloud Storage
-// All audio files over 1 minute must be from GCS for the Google Speech to Text API. 
-async function upload2GoogleBucket(localUrl, callId,lastLink, metadata,unique, cb2){
-
+// All audio files over 1 minute must be from GCS for the Google Speech to Text API.
+async function upload2GoogleBucket(
+  localUrl,
+  callId,
+  lastLink,
+  metadata,
+  unique,
+  cb2
+) {
   // Create a storage client
   const storage = new Storage();
 
   // Specify the bucket name for all files
-  const bucketName = "formatedwavfiles"
-
+  const bucketName = "formattedwavfiles";
+  console.log(localUrl);
   // Uploads a local file to tshe bucket
   await storage.bucket(bucketName).upload(localUrl, {
     // Support for HTTP requests made with `Accept-Encoding: gzip`
     gzip: false
-    /*
-    metadata: {
-      // Enable long-lived HTTP caching headers
-      // Use only if the contents of the file will never change
-      // (If the contents will change, use cacheControl: 'no-cache')
-      //cacheControl: 'public, max-age=31536000',
-     // cacheControl: 'no-transform',
-     // contentEncoding: 'gzip'
-    },
-    */
   });
 
-console.log(`${localUrl} uploaded to ${bucketName}.`);
+  console.log(`${localUrl} uploaded to ${bucketName}.`);
 
-var name = unique + ".wav"
+  var name = unique + ".wav";
 
+  // Makes the file public
+  await storage
+    .bucket(bucketName)
+    .file(name)
+    .makePublic();
 
-// Makes the file public
-await storage
-  .bucket(bucketName)
-  .file(name)
-  .makePublic();
+  console.log(`gs://${bucketName}/${name} is now public.`);
 
-console.log(`gs://${bucketName}/${name} is now public.`);
-
- googleSpeech2Text(name, callId, lastLink, metadata,unique, function(){
-  cb2()
- })
-  
+  googleSpeech2Text(name, callId, lastLink, metadata, unique, function() {
+    cb2();
+  });
 }
 
-
 // Call the google speech to text API, referencing an audio file in GCS
-async function googleSpeech2Text(name,callId, lastLink, metadata,unique, cb3) {
-  console.log("inside speech to text for id: " + name)
+async function googleSpeech2Text(
+  name,
+  callId,
+  lastLink,
+  metadata,
+  unique,
+  cb3
+) {
+  console.log("Transcribing: " + name);
 
   const audio = {
-    uri:  "gs://formatedwavfiles/" + name
+    uri: "gs://formattedwavfiles/" + name
   };
-  
+
   const request = {
     config: clientConfig,
-    audio: audio,
+    audio: audio
   };
-  
+
   // Make the API call:
   const [operation] = await client.longRunningRecognize(request);
   // Get a Promise representation of the final result of the job
   const [response] = await operation.promise();
   const transcription = response.results
     .map(result => result.alternatives[0].transcript)
-    .join('\n');
+    .join("\n");
 
   // Display the results:
   const result = response.results[response.results.length - 1];
   const wordsInfo = result.alternatives[0].words;
 
-  var sentence = ""
+  var sentence = "";
 
-  for(var i = 0; i<= wordsInfo.length - 1; i++){
-    console.log("Saving this word: " + wordsInfo[i].word)
-    sentence += wordsInfo[i].word + " "
+  for (var i = 0; i <= wordsInfo.length - 1; i++) {
+    sentence += wordsInfo[i].word + " ";
   }
 
- 
-  let temp = {}
-  temp["Unique Id"] = unique
-  temp["Call Id"] = callId
-  temp.Words = sentence
+  let temp = {};
+
+  temp["Date"] = metadata["Date"];
+  temp["Time"] = metadata["Time"];
+  temp["Employee"] = metadata["Employee"];
+  temp["Location"] = metadata["Location"];
+  temp["Extension Routing"] = metadata["Extension Routing"];
+  temp["Ad Source"] = metadata["Ad Source"];
+  temp["Routed Type"] = metadata["Routed Type"];
+  temp["Result"] = metadata["Result"];
+  temp["Duration"] = metadata["Duration"];
+  temp["Status"] = metadata["Status"];
+  temp["Tracking Number"] = metadata["Tracking Number"];
+  temp["Caller Number"] = metadata["Caller Number"];
+  temp["Target Number"] = metadata["Target Number"];
+  temp["Missed Opp"] = metadata["Missed Opp"];
+  temp["Prospect/Non-Prospect"] = metadata["Prospect/Non-Prospect"];
+  temp["Call Status"] = metadata["Call Status"];
+  temp["Call Id"] = metadata["Call Id"];
+  temp["Audio URL"] = metadata["Audio URL"];
+  temp["Campaign Id"] = metadata["Campaign Id"];
+  temp["Campaign Name"] = metadata["Campaign Name"];
+  temp["Location Code"] = metadata["Location Code"];
+  temp["Location Id"] = metadata["Location Id"];
+  temp["First Name"] = metadata["First Name"];
+  temp["Last Name"] = metadata["Last Name"];
+  temp["Street Address"] = metadata["Street Address"];
+  temp["City"] = metadata["City"];
+  temp["State"] = metadata["State"];
+  temp["Zip Code"] = metadata["Zip Code"];
+  temp["Country"] = metadata["Country"];
+  temp["Appointment Set"] = metadata["Appointment Set"];
+  temp["Appointment Date and Time"] = metadata["Appointment Date and Time"];
+  temp["Call Alert Status"] = metadata["Call Alert Status"];
+  temp.Words = sentence;
   //temp.Prospect = metadata["Prospect/Non-Prospect"]
 
-  myCSV.push(temp)
+  myCSV.push(temp);
 
-  console.log("Final: " + JSON.stringify(myCSV))
+  console.log("Final: " + JSON.stringify(myCSV));
 
   // Check if that was the last song:
-  if(lastLink){
-    //const opts ={fields}
-    const parser = new Json2csvParser({fields});
+  if (lastLink) {
+    const parser = new Json2csvParser({ fields });
     const csv = parser.parse(myCSV);
 
-    console.log("Last link confirmed!")
-
-    fs.writeFile("Transcriptions0-299.csv", csv, function(err, data) {
-      if (err) console.log(err);
-      console.log("Successfully Written to File.");
-    });
-    
-
+    fs.writeFile(
+      "../automation/transcriptions/" + process.argv[3],
+      csv,
+      function(err, data) {
+        if (err) console.log(err);
+        console.log(
+          "Successfully Written to ../automation/transcriptions/" +
+            process.argv[3]
+        );
+      }
+    );
   }
 
-  cb3()
-
-
+  cb3();
 }
 
+function convert() {
+  var qwer = "insert json array here";
 
-function convert(){
-
-  var qwer = "insert json array here"
-
-  const parser = new Json2csvParser({fields});
+  const parser = new Json2csvParser({ fields });
   const csv = parser.parse(qwer);
 
   fs.writeFile("Transcription2200-2387.csv", csv, function(err, data) {
     if (err) console.log(err);
     console.log("Successfully Written to File.");
   });
-
-
 }
-
